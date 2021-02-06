6ff95086cb52dc3d99d30131c58a74492dd0f6d9
rickfromwork <msft_richard@outlook.com>
2021-02-05 16:50:22 -0800

    undoing style change until Cedric's polyfill properly addresses setAttribute

--- gui/src/2D/controls/control.ts
+++ gui/src/2D/controls/control.ts
@@ -2134,7 +2134,7 @@ export class Control {
 
         var text = document.createElement("span");
         text.innerHTML = "Hg";
-        text.setAttribute('style', `font: ${font} !important`);
+        text.style.font = font;
 
         var block = document.createElement("div");
         block.style.display = "inline-block";
